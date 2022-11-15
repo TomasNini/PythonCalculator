d70f9782b105c1ad61fc694946bb30ba9c1258ac
TomasNini <80323992+TomasNini@users.noreply.github.com>
2022-11-15 22:40:20 +0000

    Added percentage

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,8 +1,9 @@
-
 from operator import pow, truediv, mul, add, sub, mod
 from math import sqrt
 
 operators = {
+  '+%': None,
+  '-%': None,
   'sqrt': sqrt,
   '+': add,
   '-': sub,
@@ -18,11 +19,22 @@ def calculate(s):
         left, operator, right = s.partition(c)
         if operator == 'sqrt':
             return operators[operator](calculate(right))
+        elif operator == '+%':
+            if calculate(right) < 0:
+                print('Erro! Percentagem negativa')
+            else:
+                return print("{:.2f}".format(round((100 + calculate(right))/100 * calculate(left), 0)))
+        elif operator == '-%':
+            if calculate(right) < 0:
+                print('Erro! Percentagem negativa')
+            else:
+                return print("{:.2f}".format(round((calculate(left) - calculate(right)/100 * calculate(left)), 0)))
         elif operator in operators:
             return operators[operator](calculate(left), calculate(right))
-    else:
-        print('Erro! Não insira espaços no input!')
-print("Operações disponiveis \n+ soma \n- subtração \n/ divisão \n% resto \n** potência \nsqrt raiz quadrada")
+    print('Erro! Não insira espaços no input!')
+    exit()
+print("Operações disponiveis \n+ soma \n- subtração \n/ divisão \n% resto \n** potência \nsqrt raiz quadrada "
+     "\n-% retira uma determinada % a um valor \n+% adiciona uma determinada % a um vlor")
 calc = input("Insira a conta a calcular:\n")
 print(calculate(calc))
 
